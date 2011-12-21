3ee28b8fff050b92244bb914c800b23991f0e3cb
sergevm <serge.vanmeerbeeck@gmail.com>
2011-12-21 22:53:18 +0100

    Basic scaffolding of customers and projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ProjectManagement::Application.routes.draw do
+  resources :projects
+
+  resources :customers
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ ProjectManagement::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'customers#index'
 
   # See how all your routes lay out with "rake routes"
 
