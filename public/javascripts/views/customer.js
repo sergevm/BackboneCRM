057e1657a26b896b4be6eb0cfe576845558feac8
sergevm <serge.vanmeerbeeck@gmail.com>
2011-12-24 12:33:53 +0100

    Underscore template with javascript function

--- public/javascripts/views/customer.js
+++ public/javascripts/views/customer.js
@@ -1,15 +1,43 @@
 App.Views.Customers = Backbone.View.extend({
- render: function() {
-   var list = "<ul><%_.each(customers, function(customer){%><li><%= customer.attributes.name %></li><%}) %></ul>";
-   $(this.el).html(_.template(list, {customers: this.collection.models})); 
-    $("#content").html(this.el);}
+
+  tagName: 'ul',
+  
+   template: "\
+      <% _.each(customers, function(customer) { %>\
+          <% $(list.el).append(render_customer(customer)) %>\
+      <% }) %>",
+
+  render: function() {
+    
+    var that = this;
+
+    _.template(this.template, 
+        {
+          list: that,
+          customers: this.collection.models, 
+          render_customer: this.render_customer
+        }); 
+
+    $("#content").html(this.el);
+  },
+
+  render_customer: function(customer){
+    var view = new App.Views.Customer({model: customer}); 
+    view.render();
+    return view.el;
+  }
 });
 
 App.Views.Customer = Backbone.View.extend({
+  
+  tagName: 'li',
+
   render: function() {
-    $(this.el).html(this.template({name: this.model.attributes.name}));
+    $(this.el).html(this.template(this.model.attributes));
+    return this;
   },
-  template: _.template('<li><%= name %></li>')
+
+  template: _.template('<span style="width:180"><%= name %></span>&nbsp;<span><%= legal_form %></span>')
 });
 
 
